6a25c82ef20bc3b7240c11a7b7f7020d3f37bb8e
obilovincent1234 <110498090+obilovincent1234@users.noreply.github.com>
2022-08-02 22:11:54 -0700

    Create simpleAuction.sol

---
+++ simpleAuction.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.10;
+
+contract SimpleAuction {
+    function bid() public payable {
+
+    }
+}
+
+function helper(uint x) pure returns(uint) {
+    return x = 2;
+}
